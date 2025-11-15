96e85612d75955e0d13d98748b2b62ad6f484b4b
Timotei Dumitru <dumitru.timy@gmail.com>
2025-11-15 13:02:33 +0000

    deadlock example where program freeze forever

---
+++ src/main/java/com/javaPlayground/concurrency/threads/DeadlockExample.java
@@ -0,0 +1,29 @@
+package com.javaPlayground.concurrency.threads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeadlockExample {
+
+    private final String name;
+
+    public synchronized void bow(DeadlockExample bower) {
+        System.out.println(this.name + " bows to " + bower.name);
+        bower.bowBack(this);
+    }
+
+    public synchronized void bowBack(DeadlockExample bower) {
+        System.out.println(this.name + " bows back to " + bower.name);
+    }
+
+    public static void main(String[] args) {
+        DeadlockExample a = new DeadlockExample("Alice");
+        DeadlockExample b = new DeadlockExample("Bob");
+
+        new Thread(() -> a.bow(b)).start();
+        new Thread(() -> b.bow(a)).start();
+    }
+}
+
