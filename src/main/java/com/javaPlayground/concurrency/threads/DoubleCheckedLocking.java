a8ceebad8e109280d4f4738fe371e960996cae06
Timotei Dumitru <dumitru.timy@gmail.com>
2025-11-17 16:26:00 +0000

    Double-Checked Locking concept

---
+++ src/main/java/com/javaPlayground/concurrency/threads/DoubleCheckedLocking.java
@@ -0,0 +1,18 @@
+package com.javaPlayground.concurrency.threads;
+
+public class DoubleCheckedLocking {
+
+    private static volatile DoubleCheckedLocking instance;
+
+    public static DoubleCheckedLocking get() {
+        if (instance == null) {                             // 1st check (no lock)
+            synchronized (DoubleCheckedLocking.class) {
+                if (instance == null) {                     // 2nd check
+                    instance = new DoubleCheckedLocking();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
