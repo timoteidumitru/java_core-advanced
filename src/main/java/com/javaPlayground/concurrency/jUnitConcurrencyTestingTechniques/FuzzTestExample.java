56788261f27039f71e11681438cb71731afd7a0b
Timotei Dumitru <dumitru.timy@gmail.com>
2025-12-22 23:01:56 +0000

    Random delays + interrupts test example

---
+++ src/main/java/com/javaPlayground/concurrency/jUnitConcurrencyTestingTechniques/FuzzTestExample.java
@@ -0,0 +1,35 @@
+package com.javaPlayground.concurrency.jUnitConcurrencyTestingTechniques;
+
+import java.util.Random;
+
+public class FuzzTestExample {
+
+    static volatile boolean running = true;
+
+    public static void main(String[] args) throws Exception {
+        Random random = new Random();
+
+        Thread worker = new Thread(() -> {
+            while (running) {
+                try {
+                    Thread.sleep(random.nextInt(5));
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted!");
+                }
+            }
+        });
+
+        worker.start();
+
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(random.nextInt(3));
+            worker.interrupt();
+        }
+
+        running = false;
+        worker.join();
+
+        System.out.println("Fuzz test completed");
+    }
+}
+
